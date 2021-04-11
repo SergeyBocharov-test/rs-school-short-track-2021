0b87264bf7ba84c317586f56b87f7d44a3fe9929
SergeyBocharov-test <work.sergey.bocharov@ya.ru>
2021-04-11 12:30:17 +0300

    feat: implemented deleteDigit function
    5/15

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,14 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  let maximalNumber = 0;
+  for (let i = 0; i < n.toString().length; i++) {
+    const nSplit = n.toString().split('');
+    nSplit.splice(i, 1);
+    if (+nSplit.join('') > maximalNumber) maximalNumber = +nSplit.join('');
+  }
+  return maximalNumber;
 }
 
 module.exports = deleteDigit;
