d5956817c1eb6400db3bef34e7ed542a47f90858
SergeyBocharov-test <work.sergey.bocharov@ya.ru>
2021-04-11 15:58:01 +0300

    feat: implemented renameFiles function
    7/15

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,22 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(fileNamesList) {
+  const fileNamesListValidated = fileNamesList;
+  for (let i = 0; i < fileNamesList.length; i++) {
+    const comparisonFileName = fileNamesList[i];
+    let counter = 0; // suffix k
+    for (let f = 0; f < fileNamesList.length; f++) {
+      const comparedFileName = fileNamesList[f];
+      if (f !== i) {
+        if (comparedFileName === comparisonFileName) {
+          counter++;
+          fileNamesListValidated[f] += `(${counter})`; // apply suffix
+        }
+      }
+    }
+  }
+  return fileNamesListValidated;
 }
 
 module.exports = renameFiles;
