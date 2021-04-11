f5cbcd990f2230a8877ff38fbf74f4e60a4afa09
SergeyBocharov-test <work.sergey.bocharov@ya.ru>
2021-04-11 11:58:26 +0300

    feat: implemented minesweeper function
    4/15

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,34 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const matrixNew = [];
+  for (let i = 0; i < matrix.length; i++) {
+    matrixNew.push(new Array(matrix[i].length).fill(0));
+  }
+  if (matrixNew.length > 0) {
+    const rows = matrix.length - 1;
+    const columns = matrix[0].length - 1;
+    for (let r = 0; r <= rows; r++) {
+      for (let c = 0; c <= columns; c++) {
+        if (matrix[r][c]) {
+          if (r > 0) {
+            if (c > 0) matrixNew[r - 1][c - 1]++;
+            matrixNew[r - 1][c]++;
+            if (c < columns) matrixNew[r - 1][c + 1]++;
+          }
+          if (c > 0) matrixNew[r][c - 1]++;
+          if (c < columns) matrixNew[r][c + 1]++;
+          if (r < rows) {
+            if (c > 0) matrixNew[r + 1][c - 1]++;
+            matrixNew[r + 1][c]++;
+            if (c < columns) matrixNew[r + 1][c + 1]++;
+          }
+        }
+      }
+    }
+  }
+  return matrixNew;
 }
 
 module.exports = minesweeper;
