f3dcf364aaf93e3141493eb30429dd83f32ac070
SergeyBocharov-test <work.sergey.bocharov@ya.ru>
2021-04-12 19:07:30 +0300

    feat: implemented getEmailDomain function
    13/15
    skipped: 08-remove-from-list, 14-queue

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,11 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  // if email would have been legit email data type - following code would have work:
+  // return email.split('').splice(email.indexOf('@') + 1, email.length - 1).join('');
+  const emailSplit = email.split('@');
+  return emailSplit[emailSplit.length - 1];
 }
 
 module.exports = getEmailDomain;
