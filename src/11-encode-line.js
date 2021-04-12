b161f8d9c8f347040c05daa1abdcbd43cc1258c6
SergeyBocharov-test <work.sergey.bocharov@ya.ru>
2021-04-12 07:39:22 +0300

    feat: implemented encodeLine function
    10/15
    skipped: 08-remove-from-list

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+// str - string
+function encodeLine(string) {
+  const stringEncoded = [];
+  let counter = 1;
+  for (let i = 0; i < string.length; i++) {
+    // console.log('- - -');
+    // console.log(`i : ${i} >> ${string[i]}`);
+    if (string[i] === string[i + 1]) {
+      counter++;
+    } else {
+      if (counter > 1) {
+        stringEncoded.push(`${counter}${string[i]}`);
+      } else stringEncoded.push(`${string[i]}`);
+      counter = 1;
+    }
+    // console.log(`counter : ${counter}`);
+  }
+  return stringEncoded.join('');
 }
 
 module.exports = encodeLine;
