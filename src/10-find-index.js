4a6a9e1438f8392d02cf37c03a96c9c3915b5532
SergeyBocharov-test <work.sergey.bocharov@ya.ru>
2021-04-12 06:04:03 +0300

    feat: implemented findIndex function
    9/15
    skipped: 08-remove-from-list

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,42 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  // v2 - 16ms
+  let head = 0;
+  let tail = array.length - 1;
+  let middle;
+  let index = -1;
+  let found = false;
+  while (found === false && head <= tail) {
+    middle = Math.floor((head + tail) / 2);
+    if (array[middle] === value) {
+      found = true;
+      index = middle;
+    }
+    if (array[middle] > value) {
+      tail = middle - 1;
+    } else head = middle + 1;
+  }
+  return index;
 }
 
 module.exports = findIndex;
+
+/*
+ *  v1 - 632ms
+ *  function binarySearchForIndex(arr, headIndex) {
+ *    const middle = Math.floor(arr.length / 2);
+ *    if (arr[middle] === value) {
+ *      return middle + headIndex;
+ *    }
+ *    if (arr[middle] < value && arr.length > 1) {
+ *      return binarySearchForIndex(arr.splice(middle, arr.length - middle), middle + headIndex);
+ *    }
+ *    if (arr[middle] > value && arr.length > 1) {
+ *      return binarySearchForIndex(arr.splice(0, middle), headIndex);
+ *    }
+ *    return -1;
+ *  }
+ *  return binarySearchForIndex(array, 0);
+ */
