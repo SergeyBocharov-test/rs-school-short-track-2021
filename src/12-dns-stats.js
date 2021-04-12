69dc9bfd4667915e3d1d1d74917bf80cea26411d
SergeyBocharov-test <work.sergey.bocharov@ya.ru>
2021-04-12 18:04:55 +0300

    feat: implemented getDNSStats function
    11/15
    skipped: 08-remove-from-list

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,50 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  // console.log('domains :');
+  // console.log(domains);
+
+  if (domains.length === 0) return {};
+
+  const objectDNS = {}; // result
+  for (let i = 0; i < domains.length; i++) {
+    // console.log(`= = = =`)
+    // console.log(`i : ${i} :: ${domains[i]}`)
+    const domainStorage = []; // temp storage for domain names
+    let domainCollector = []; // temp domain name accumulator
+    let domainTreeCollector = ''; // temp domain tree accumulator
+
+    for (let j = domains[i].length - 1; j > -1; j--) {
+      // console.log(`- - - -`);
+      // console.log(`j : ${j} :: ${domains[i][j]}`);
+      if (domains[i][j] !== ('.')) {
+        domainCollector.push(domains[i][j]);
+        // console.log(`domainCollector : ${domainCollector}`);
+      }
+      // console.log (`not a dot or the head - ${domains[i][j] === ('.') || j === 0}`);
+      if (domains[i][j] === ('.') || j === 0) {
+        domainTreeCollector += `.${domainCollector.reverse().join('')}`;
+        domainStorage.push(domainTreeCollector);
+        domainCollector = [];
+        // console.log(`domainTreeCollector : ${domainTreeCollector}`);
+        // console.log(`domainStorage : ${domainStorage}`);
+      }
+    }
+    // console.log('* * *')
+    /* Writing accumulated domains to objectDNS */
+    for (let k = 0; k < domainStorage.length; k++) {
+      // console.log(`domainStorage[${k}] : ${domainStorage[k]}`)
+      if (!Object.prototype.hasOwnProperty.call(objectDNS, domainStorage[k])) {
+        objectDNS[domainStorage[k]] = 1;
+      } else {
+        objectDNS[domainStorage[k]]++;
+      }
+      // console.log(objectDNS)
+    }
+  }
+  // console.log(`result :`)
+  return objectDNS;
 }
 
 module.exports = getDNSStats;
